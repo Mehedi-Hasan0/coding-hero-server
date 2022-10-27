bce8fa7aae6dff6f63511407f27817d5a8d90e10
Mehedi-Hasan0 <developermahedi.me@gmail.com>
2022-10-27 11:12:31 +0600

    course data collection completed

--- index.js
+++ index.js
@@ -5,7 +5,8 @@ const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const sideCategories = require('./data/sideCategories.json')
+const sideCategories = require('./data/sideCategories.json');
+const courses = require('./data/courses.json');
 
 app.get('/', (req, res) => {
     res.send('coding api running');
@@ -13,7 +14,23 @@ app.get('/', (req, res) => {
 
 app.get('/sideCategories', (req, res) => {
     res.send(sideCategories);
-})
+});
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    const category_courses = courses.filter(n => n.id === id);
+    res.send(category_courses)
+});
+
+app.get('/courses', (req, res) => {
+    res.send(courses);
+});
+
+app.get('/courses/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedCourse = courses.find(n => n.id === id);
+    res.send(selectedCourse);
+});
 
 app.listen(port, () => {
     console.log('coding hero server is running on port:', port);
